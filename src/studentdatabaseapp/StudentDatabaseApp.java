ce3d9968564e1bd8585d059823424a9c789e5d17
Glen R. Vasa <glenvasa@gmail.com>
2022-03-29 09:52:11 -0400

    Initial commit

---
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,13 @@
+package studentdatabaseapp;
+
+public class StudentDatabaseApp {
+
+	public static void main(String[] args) {
+		// Ask how many users we want to add
+		
+		// Create n number of new students
+		new Student();
+
+	}
+
+}
